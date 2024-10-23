d9d7f4bd95fb90c146951cb24d991208b4e32423
wayne <waynemortenson@gmail.com>
2024-10-22 21:18:42 -0500

    Update main.go

--- Go/Lets Go/code/snippetbox/main.go
+++ Go/Lets Go/code/snippetbox/main.go
@@ -11,10 +11,19 @@ func home(w http.ResponseWriter, r *http.Request){
 	log.Print("Serving data")
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request){
+	log.Print("Serving snippet view")
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request){
+	log.Print("Serving snippet create")
+}
+
 func main(){
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	log.Print("Starting server on :8000")
 
 	err := http.ListenAndServe(":8000", mux)
